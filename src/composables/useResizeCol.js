b4b54f4fd90098c59b0c4ef7844b1333118ab79a
Ivan G <mr.zero@inbox.ru>
2024-04-17 20:39:30 +0300

    fix: limit minimal width for resizeable columns

--- src/composables/useResizeCol.js
+++ src/composables/useResizeCol.js
@@ -1,32 +1,36 @@
-import { ref } from 'vue'
+import { ref } from "vue";
 import { useMouse, useElementBounding } from "@vueuse/core";
+
+const MIN_WIDTH = 70;
 
 export default function useResizeCol(thRefs, columns) {
   const isResizing = ref(null);
 
-  const i = ref(null)
+  const i = ref(null);
 
   const { x } = useMouse();
 
   const startResize = (col) => {
     isResizing.value = col;
   };
-  
+
   const handleResize = () => {
     if (!isResizing.value) {
       return;
     }
+
     const { left } = useElementBounding(thRefs.value[isResizing.value.i]);
     const col = columns.value.find(
       (column) => column.key === isResizing.value.key
     );
-    col.width = x.value - left.value;
+    if (col.editable) {
+      col.width = x.value - left.value < MIN_WIDTH ? MIN_WIDTH : x.value - left.value;
+    }
   };
-  
+
   const stopResize = () => {
     isResizing.value = null;
   };
 
-
-  return { startResize, handleResize, stopResize, isResizing }
+  return { startResize, handleResize, stopResize, isResizing };
 }
